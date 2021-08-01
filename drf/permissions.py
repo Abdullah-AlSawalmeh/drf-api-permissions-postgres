e3683d939b98caefe293426128beaaca6a1047ec
abdullah-alsawalmeh <abdullahalsawalmeh@gmail.com>
2021-08-01 04:06:57 +0300

    Finish lab 32

--- drf/permissions.py
+++ drf/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
     def has_object_permission(self, request, view, obj):
         # Read Only permissions
         if request.method in permissions.SAFE_METHODS:
